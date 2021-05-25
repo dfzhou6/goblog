5fb7c3346154a16b9aefa02f7f2f144ef4f6e6f4
dfzhou6 <1371105743@qq.com>
2021-05-25 16:02:43 +0800

    完善articles/crud

--- app/models/article/crud.go
+++ app/models/article/crud.go
@@ -1,6 +1,7 @@
 package article
 
 import (
+	"github.com/dfzhou6/goblog/pkg/logger"
 	"github.com/dfzhou6/goblog/pkg/model"
 	"github.com/dfzhou6/goblog/pkg/pagination"
 	"github.com/dfzhou6/goblog/pkg/route"
@@ -22,4 +23,46 @@ func GetAll(r *http.Request, perPage int) (cs []Article, data pagination.ViewDat
 	viewData := _pager.Paging()
 	_pager.Results(&cs)
 	return cs, viewData, nil
+}
+
+func (a *Article) Create() (err error) {
+	if err = model.DB.Create(&a).Error; err != nil {
+		logger.LogError(err)
+		return err
+	}
+	return nil
+}
+
+func (a *Article) Update() (rowsAffected int64, err error) {
+	result := model.DB.Save(&a)
+	if err = result.Error; err != nil {
+		logger.LogError(err)
+		return 0, err
+	}
+	return result.RowsAffected, nil
+}
+
+func (a *Article) Delete() (rowsAffected int64, err error) {
+	result := model.DB.Delete(&a)
+	if err = result.Error; err != nil {
+		logger.LogError(err)
+		return 0, err
+	}
+	return result.RowsAffected, nil
+}
+
+func GetByUserID(uid string) (as []Article, err error) {
+	if err = model.DB.Where("user_id = ?", uid).Preload("User").Find(&as).Error; err != nil {
+		return as, err
+	}
+	return as, nil
+}
+
+func GetByCategoryID(cid string, r *http.Request, perPage int) ([]Article, pagination.ViewData, error) {
+	db := model.DB.Model(Article{}).Where("category_id = ?", cid).Order("created_at desc")
+	_pager := pagination.New(r, db, route.Name2URL("articles.index"), perPage)
+	viewDta := _pager.Paging()
+	var articles []Article
+	_pager.Results(&articles)
+	return articles, viewDta, nil
 }
