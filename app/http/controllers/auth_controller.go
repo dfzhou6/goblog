2fcccce13c2e8aa41ce07d56a4b6cf13056e91f1
dfzhou6 <1371105743@qq.com>
2021-05-31 20:35:06 +0800

    完善user控制器

--- app/http/controllers/auth_controller.go
+++ app/http/controllers/auth_controller.go
@@ -1,8 +1,10 @@
 package controllers
 
 import (
+	"fmt"
 	"github.com/dfzhou6/goblog/app/models/user"
 	"github.com/dfzhou6/goblog/app/requests"
+	"github.com/dfzhou6/goblog/pkg/auth"
 	"github.com/dfzhou6/goblog/pkg/flash"
 	"github.com/dfzhou6/goblog/pkg/view"
 	"net/http"
@@ -34,6 +36,37 @@ func (*AuthController) DoRegister(w http.ResponseWriter, r *http.Request) {
 		_user.Create()
 		if _user.ID > 0 {
 			flash.Success("恭喜您注册成功")
+			auth.Login(_user)
+			http.Redirect(w, r, "/", http.StatusFound)
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprint(w, "创建用户失败，请联系管理员")
 		}
 	}
+}
+
+func (*AuthController) Login(w http.ResponseWriter, r *http.Request) {
+	view.RenderSimple(w, view.D{}, "auth.login")
+}
+
+func (*AuthController) DoLogin(w http.ResponseWriter, r *http.Request) {
+	email := r.PostFormValue("email")
+	password := r.PostFormValue("password")
+
+	if err := auth.Attempt(email, password); err == nil {
+		flash.Success("欢迎回来")
+		http.Redirect(w, r, "/", http.StatusFound)
+	} else {
+		view.RenderSimple(w, view.D{
+			"Error":    err.Error(),
+			"Email":    email,
+			"Password": password,
+		}, "auth.login")
+	}
+}
+
+func (*AuthController) Logout(w http.ResponseWriter, r *http.Request) {
+	auth.Logout()
+	flash.Success("您已退出登录")
+	http.Redirect(w, r, "/", http.StatusFound)
 }
